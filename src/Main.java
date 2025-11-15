844f384eedd8a11bd4b5485afff18aaaaf11fea8
d-evm <devamsonigra@gmail.com>
2025-11-15 14:43:25 +0530

    for testing

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,14 @@
+import jsonparser.lexer.*;
+
 public class Main {
+    public static void main(String[] args) {
+        String json = "{ \"a\": 10, \"b\": [1,2] }";
+        Lexer lexer = new Lexer(json);
+
+        Token token;
+        do {
+            token = lexer.nextToken();
+            System.out.println(token);
+        } while (token.getType() != TokenType.EOF);
+    }
 }
